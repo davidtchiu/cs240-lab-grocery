88a7e7535b3150e11a73b7d5b69fd36fec1b318b
David Chiu <david.t.chiu@gmail.com>
2021-10-03 11:09:21 -0700

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-// add an event for button press
-let buttonNode = document.querySelector("#addButton");
-buttonNode.addEventListener("click", function () {
-  let inputField = document.querySelector("input[name='itemName']");
-
-  // get the value from the input field
-  addItem("#list", inputField.value);
-
-  // clear out field
-  inputField.value = "";
-});
-
 // add an event for keypress
 let inputNode = document.querySelector("input[name='itemName']");
-inputNode.addEventListener("keypress", function (evt) {
+inputNode.addEventListener("keydown", function (evt) {
+  // need to check if the return key was pressed
   if (evt.code === "Enter") {
     // get the value from the input field
     addItem("#list", inputNode.value);
@@ -32,42 +21,15 @@ function addItem(listID, item) {
     listID = "#" + listID;
   }
 
-  // try to find existing item (case insensitive)
-  let found = false;
+  // create a <span>item name</span>
+  let itemName = document.createElement("span");
+  itemName.innerHTML = item;
+
+  // nest the <span> in <li>...</li>
+  let newListItem = document.createElement("li");
+  newListItem.appendChild(itemName);
+
+  // append the <li> to the end of the list
   let ul = document.querySelector(listID);
-  for (let i = 0; i < ul.childElementCount; i++) {
-    // first child is qty, last child is item name
-    if (ul.children[i].lastChild.innerHTML == item) {
-      let cnt = parseInt(ul.children[i].firstChild.innerHTML) + 1;
-      ul.children[i].firstChild.innerHTML = cnt;
-      found = true;
-    }
-  }
-  if (!found) {
-    // create children of item node
-    // let qty = document.createElement("span");
-    // qty.innerHTML = 1;
-    let itemName = document.createElement("span");
-    itemName.innerHTML = item;
-
-    // make a new checkbox
-    let chkbox = document.createElement("input");
-    chkbox.type = "checkbox";
-    chkbox.addEventListener("change", function () {
-      if (chkbox.checked) {
-        chkbox.parentElement.className = "itemChecked";
-      } else {
-        chkbox.parentElement.className = "";
-      }
-    });
-
-    let newListItem = document.createElement("li");
-    newListItem.appendChild(chkbox);
-    // newListItem.appendChild(qty);
-    newListItem.appendChild(itemName);
-
-    // append to the list
-    let ul = document.querySelector(listID);
-    ul.appendChild(newListItem);
-  }
+  ul.appendChild(newListItem);
 }
