055337542cf33fd3828803a6f66363c5dbaecfe0
David Chiu <david.t.chiu@gmail.com>
2021-10-05 10:47:02 -0700

    removed function

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
-// add an event for keypress
+/** add an event for keydown */
 let inputNode = document.querySelector("input[name='itemName']");
 inputNode.addEventListener("keydown", function (evt) {
-  // need to check if the return key was pressed
+  // need to check if the return key was depressed
   if (evt.code === "Enter") {
-    // get the value from the input field
-    addItem("#list", inputNode.value);
+    // create a <span>value from input field</span>
+    let itemName = document.createElement("span");
+    itemName.innerHTML = inputNode.value;
 
-    // clear out field
+    // nest the <span> in <li>...</li>
+    let newListItem = document.createElement("li");
+    newListItem.appendChild(itemName);
+
+    // append the <li> to the end of the list
+    let ul = document.querySelector("#list");
+    ul.appendChild(newListItem);
+
+    // clear out the input field
     inputNode.value = "";
   }
 });
-
-/**
- * Adds an item to the list
- * @param {string} listID HTML id of the list
- * @param {string} item Name of the grocery list item to be added
- */
-function addItem(listID, item) {
-  if (!listID.startsWith("#")) {
-    listID = "#" + listID;
-  }
-
-  // create a <span>item name</span>
-  let itemName = document.createElement("span");
-  itemName.innerHTML = item;
-
-  // nest the <span> in <li>...</li>
-  let newListItem = document.createElement("li");
-  newListItem.appendChild(itemName);
-
-  // append the <li> to the end of the list
-  let ul = document.querySelector(listID);
-  ul.appendChild(newListItem);
-}
